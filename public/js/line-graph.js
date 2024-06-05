b0ca95e5445f23b59c7457c3897326c30c4e4313
rvanetta97 <rachel.vanetta@gmail.com>
2024-06-04 21:17:04 -0400

    chart.js changes

--- public/js/line-graph.js
+++ public/js/line-graph.js
@@ -1,33 +1,66 @@
-import Chart from 'chart.js/auto'
+// import Chart from 'chart.js/auto'
 
-(async function () {
+// (async function () {
 
-    const labels = [
-        'Monday',
-        'Tuesday',
-        'Wednesday',
-        'Thursday',
-        'Friday',
-        'Saturday',
-        'Sunday'
-    ];
+//     const labels = [
+//         'Monday',
+//         'Tuesday',
+//         'Wednesday',
+//         'Thursday',
+//         'Friday',
+//         'Saturday',
+//         'Sunday'
+//     ];
 
-    new Chart(
-        document.getElementById('line-graph'),
-        {
-            type: 'line',
-            data: {
-                labels: labels,
-                datasets: [
-                    {
-                        label: 'Calories Burned',
-                        data: [150, 300, 275, 180, 157, 203, 216],
-                        fill: false,
-                        borderColor: 'rgb(101, 170, 94)',
-                        tension: 0.1
-                    }
-                ]
+//     new Chart(
+//         document.getElementById('line-graph'),
+//         {
+//             type: 'line',
+//             data: {
+//                 labels: labels,
+//                 datasets: [
+//                     {
+//                         label: 'Calories Burned',
+//                         data: [150, 300, 275, 180, 157, 203, 216],
+//                         fill: false,
+//                         borderColor: 'rgb(101, 170, 94)',
+//                         tension: 0.1
+//                     }
+//                 ]
+//             }
+//         }
+//     );
+// })();
+
+document.addEventListener('DOMContentLoaded', () => {
+    // Line Chart
+
+        const labels = [
+            'Monday',
+            'Tuesday',
+            'Wednesday',
+            'Thursday',
+            'Friday',
+            'Saturday',
+            'Sunday'
+        ];
+    
+        new Chart(
+            document.getElementById('line-graph'),
+            {
+                type: 'line',
+                data: {
+                    labels: labels,
+                    datasets: [
+                        {
+                            label: 'Calories Burned',
+                            data: [150, 300, 275, 180, 157, 203, 216],
+                            fill: false,
+                            borderColor: 'rgb(101, 170, 94)',
+                            tension: 0.1
+                        }
+                    ]
+                }
             }
-        }
-    );
+        );
 })();
